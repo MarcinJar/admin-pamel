0de63e1c22846083462c1d2da691646cf6e96dff
Marcin Jarmułowski <m.jarmulowski@gmail.com>
2024-09-22 17:10:01 +0200

    index.js layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
+  corePlugins: {
+    preflight: false,
+    container: false,
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      "2xl": "1536px",
+    },
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
