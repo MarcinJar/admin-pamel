85116e715d562cebb39eb5985638a139c2df2514
Marcin Jarmułowski <m.jarmulowski@gmail.com>
2024-09-23 14:50:52 +0200

    Add DefaultLayout

---
+++ components/layouts/defaultLayout/DefaultLayout.js
@@ -0,0 +1,27 @@
+export default function DefaultLayout({children, title = ''}) {
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="md:flex">
+        WIERSZ GÓRNY
+      </div>
+      <div className="flex flex-grow overflow-hidden">
+        <div className="hidden md:flex items-start justify-between px-8 py-8 bg-blue-800
+          text-blue-100 md:flex-shrink-0 md:w-56 md:justify-start">
+          <dev className="relative">
+            KOLUMNA BOCZNA
+          </dev>
+        </div>
+        <div className="w-full overflow-hidden overflow-y-auto p-0 bg-gray=100 py-0 md:py-8">
+          <div className="p-4 md:py-0 md:px-12">
+            <h2 className="text-2xl mb-3">
+              {title}
+            </h2>
+            <section>
+              {children}
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ pages/admin/index.js
@@ -0,0 +1,7 @@
+import DefaultLayout from "@/components/layouts/defaultLayout/DefaultLayout";
+
+export default function Index() {
+  return (
+    <DefaultLayout title="Panel administaratora"/>
+  )
+}
